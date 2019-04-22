883f7adbaf811f59f41d13b4b2bc3441ccf3238b
youfang <981545521@qq.com>
2019-04-22 15:50:15 +0800

    add : schedule

--- gemini-main/src/main/java/cn/acyou/gemini/GeminiApplication.java
+++ gemini-main/src/main/java/cn/acyou/gemini/GeminiApplication.java
@@ -2,12 +2,14 @@ package cn.acyou.gemini;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author youfang
  * @version [1.0.0, 2019-04-12 上午 09:31]
  **/
 @SpringBootApplication
+@EnableScheduling
 public class GeminiApplication {
 
     public static void main(String[] args) {
---
+++ gemini-schedule/src/main/java/cn/acyou/gemini/schedule/SecuritiesTimesService.java
@@ -0,0 +1,23 @@
+package cn.acyou.gemini.schedule;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author youfang
+ * @version [1.0.0, 2019-04-22 下午 03:32]
+ **/
+@Slf4j
+@Component
+public class SecuritiesTimesService {
+
+    public SecuritiesTimesService() {
+        log.info("SecuritiesTimesService 初始化");
+    }
+
+    @Scheduled(cron = "0 */30 * * * ?")
+    public void hushenA(){
+        log.info("—————— 每5秒执行一次 ——————");
+    }
+}
